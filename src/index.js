4a682c0aa65588c6b9fc8bef4cac67b71faa1bff
sla6ak <slabakxaker@gmail.com>
2022-01-07 13:55:08 +0200

    card img

--- src/index.js
+++ src/index.js
@@ -29,23 +29,27 @@ function htmlImg(arrImg) {
   return arrImg.map(img => {
     console.log(arrImg);
     return `<div class="photo-card">
-           <a class="gallery__item" href="${img.largeImageURL}">
-            <img class="gallery__image" src="${img.largeImageURL}" loading="lazy title ="" alt="" />
-            </a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b>
-          </p>
-          <p class="info-item">
-            <b>Views</b>
-          </p>
-          <p class="info-item">
-            <b>Comments</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>
-          </p>
-        </div>
-      </div>`;
+                <a class="gallery__item" href="${img.largeImageURL}">
+                  <img class="gallery__image" src="${img.webformatURL}" loading="lazy title ="" alt="" />
+                </a>
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b>
+                        ${img.likes}
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b>
+                        ${img.views}
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b>
+                        ${img.comments}
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b>
+                        ${img.downloads}
+                    </p>
+                </div>
+            </div>`;
   });
 }
