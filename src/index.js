2f88831e657ad02e49b357692c4f21e18c856ca8
sla6ak <slabakxaker@gmail.com>
2022-01-10 15:54:31 +0200

    class fetch

--- src/index.js
+++ src/index.js
@@ -1,21 +1,75 @@
 import './css/styles.css';
 import axios from 'axios';
-import { clientMessage } from './js/messageNotify';
+import Notiflix from 'notiflix';
+import { SendImg } from './js/classOOP';
+// import simpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+import { htmlImg } from './js/htmlRender';
 
 const render = document.querySelector('#render');
 const more = document.querySelector('.load-more');
 document.querySelector('#search-form').addEventListener('submit', onFetchForm);
-let numberPages = 1;
+let arrImg = [];
 
-function onFetchForm(e) {
+const sendImg = new SendImg();
+
+async function onFetchForm(e) {
   e.preventDefault();
-  const send = document.querySelector('#inputimg').value;
-  console.log(send);
-  numberPages = 1;
-  more.addEventListener('click', onMoreClick);
-  clientMessage(send, numberPages);
+  let valueInput = document.querySelector('#inputimg').value;
+  if (valueInput === '') {
+    remove(render);
+  } else {
+    remove(render);
+    sendImg.startPages();
+    sendImg.metodSend = valueInput;
+    try {
+      arrImg = await sendImg.fetchImg(sendImg.metodSend, sendImg.numberPages);
+    } catch {
+      Notiflix.Notify.error();
+      `Error fetch`;
+    }
+
+    addImg(arrImg);
+
+    more.addEventListener('click', onMoreClick);
+    async function onMoreClick() {
+      console.log(sendImg.numberPages);
+      sendImg.nextPages();
+      console.log(sendImg.numberPages);
+      let arrImg = await sendImg.fetchImg(sendImg.metodSend, sendImg.numberPages);
+      render.insertAdjacentHTML('beforeend', htmlImg(arrImg.hits).join(''));
+    }
+  }
+  //   // clientMessage(send, sendImg.numberPages);
 }
-function onMoreClick() {
-  numberPages += 1;
-  clientMessage(send, numberPages);
+
+function addImg(arrImg) {
+  render.insertAdjacentHTML('beforeend', htmlImg(arrImg.hits).join(''));
 }
+function remove(Element) {
+  Element.innerHTML = '';
+}
+// **************************************************
+// function clientMessage() {
+//   sendImg
+//     .fethcImg(this.send, this.numberPages)
+//     .then(arrImg => {
+//       if (arrImg.hits.length === 0) {
+//         Notiflix.Notify.warning(`Hooray! We found ${0} images.`);
+//         return;
+//       }
+//       console.log(arrImg);
+//       Notiflix.Notify.success(`Hooray! We found ${arrImg.totalHits} images.`);
+//       render.innerHTML = htmlImg(arrImg.hits).join('');
+//       let gallerySet = new simpleLightbox('.gallery__item', {
+//         captionPosition: 'bottom',
+//         captionDelay: 250,
+//       });
+//       gallerySet.on('show.simplelightbox', function () {
+//         console.log(numberPages);
+//       });
+//     })
+//     .catch(error => {
+//       Notiflix.Notify.error("We're sorry, but you've reached the end of search results.");
+//     });
+// }
---
+++ src/js/classOOP.js
@@ -0,0 +1,39 @@
+export class SendImg {
+  constructor() {
+    this.send = '';
+    this.numberPages = 1;
+  }
+  async fetchImg(send, numberPages) {
+    const meta = new URLSearchParams({
+      key: '25142623-5ec88ba8c20545ff15079e1b4',
+      q: this.send,
+      orientation: 'horizontal',
+      safesearch: true,
+      image_type: 'photo',
+      page: this.numberPages,
+      per_page: 4,
+    });
+    const url = `https://pixabay.com/api/?${meta}`;
+    const res = await fetch(url);
+    return res.json();
+  }
+
+  get metodSend() {
+    return this.send;
+  }
+
+  set metodSend(newSend) {
+    if (newSend === '') {
+      console.error('Ошибка! Почта не может быть пустой строкой!');
+      return;
+    }
+    this.send = newSend;
+  }
+
+  nextPages() {
+    this.numberPages += 1;
+  }
+  startPages() {
+    this.numberPages = 1;
+  }
+}
